3f56f6f0f7588ee52a069d401a404a5f103027d7
Nissim Nabar <nissimnabar@gmail.com>
2015-08-04 23:53:48 +0530

    Naive RF solution (AUC 0.865, rank 856)

--- amazon_employee_access_kaggle.R
+++ amazon_employee_access_kaggle.R
@@ -6,5 +6,15 @@ train <- read.csv("~/Desktop/Amazon_Employee_Access/train.csv", header = TRUE,
 test <- read.csv("~/Desktop/Amazon_Employee_Access/test.csv", header = TRUE, 
                   stringsAsFactors = FALSE)
 
-colnames(train)
-View(head(train))
+# Naive Random Forest (without any Feature Selection) # Score = 0.861 rank = 856
+library(randomForest)
+forest.model = randomForest(as.factor(ACTION) ~.,data=train,ntree=100)
+forest.prediction = predict(forest.model,newdata =test[-1],type="prob",predict.all=TRUE)
+View(head(forest.prediction))
+
+submission <- cbind(test$id,as.data.frame(forest.prediction$aggregate[,2]))
+colnames(submission) <- c("id","ACTION")
+
+write.csv(submission, file = "~/Desktop/Amazon_Employee_Access/my_submission_naive_RF.csv",row.names = FALSE)
+
+# 
